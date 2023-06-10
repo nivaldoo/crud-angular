96ada492921d09497ca98ebbfc5fe8cc0a8ae609
unknown <qualquer>
2023-06-10 20:46:31 -0300

    Adição de Courses e Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'course' },
+  { path: 'course', loadChildren: () => import('./course/course.module').then(m => m.CourseModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
