b4ec8cc8df93c046ef4532784b14f530bf3e1871
unknown <qualquer>
2023-06-12 22:33:21 -0300

    Video 20

--- src/app/course/course-form/course-form.component.ts
+++ src/app/course/course-form/course-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { CoursesService } from '../services/courses.service';
+import { EMPTY, catchError } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-course-form',
@@ -10,7 +13,10 @@ export class CourseFormComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private snackBar: MatSnackBar) {
     this.form = this.formBuilder.group({
       name: [null],
       category: [null]
@@ -21,11 +27,21 @@ export class CourseFormComponent implements OnInit {
   }
 
   onSubmit() {
-
+    this.service.save(this.form.value).pipe(
+      catchError(err => {
+        this.onError();
+        return EMPTY;
+      })).subscribe(
+        next => console.log(next)
+      );
   }
 
   onCancel() {
 
+  }
+
+  private onError(){
+    this.snackBar.open('Erro ao salvar o curso.', '', { duration: 2000 });
   }
 
 }
